7995db2a9c305f6e8174c78a00d28f8f4c4e4566
C3h1c <fivemsloofficial@gmail.com>
2023-04-02 11:20:15 +0200

    Locale SL
    Co-Authored-By: cehic <98315104+cehic@users.noreply.github.com>

--- esx_accessories.sql
+++ esx_accessories.sql
@@ -1,6 +1,6 @@
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('user_ears', 'Ears', 0),
-	('user_glasses', 'Glasses', 0),
-	('user_helmet', 'Helmet', 0),
-	('user_mask', 'Mask', 0)
+	('user_ears', 'Usesa', 0),
+	('user_glasses', 'Ocala', 0),
+	('user_helmet', 'Klobuk', 0),
+	('user_mask', 'Maska', 0)
 ;
