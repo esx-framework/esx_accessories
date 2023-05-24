0d32285782b9669ab1c9c25fbb8105ab4628c32d
YOMAN1792 <94007829+YOMAN1792@users.noreply.github.com>
2023-05-24 17:46:21 -0400

    Create fr_esx_accessories.sql

---
+++ localization/fr_esx_accessories.sql
@@ -0,0 +1,6 @@
+INSERT INTO `datastore` (name, label, shared) VALUES
+    ('user_ears', 'Oreilles', 0),
+    ('user_glasses', 'Lunettes', 0),
+    ('user_helmet', 'Casque', 0),
+    ('user_mask', 'Masque', 0)
+;
